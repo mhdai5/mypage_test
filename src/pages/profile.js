60212938b82a91eaa81e8169250869fb879fc8e7
mhdai5 <mhdai5813@yahoo.co.jp>
2023-09-18 22:28:42 +0900

    my first commit

---
+++ src/pages/profile.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+import { onAuthStateChanged } from 'firebase/auth';
+
+const auth = getAuth();
+
+const ProfilePage = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <div>
+      {user ? <p>User ID: {user.uid}</p> : <p>Loading...</p>}
+    </div>
+  );
+};
+
+export default ProfilePage;
